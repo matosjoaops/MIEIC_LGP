a24492407616ad8e4ab31eb020df497e2d851bea
matosjoaops <matosjoaops@protonmail.com>
2022-04-12 18:15:15 +0100

    Have a semi functional connection using neode

--- neo4j/src/index.js
+++ neo4j/src/index.js
@@ -1,32 +1,36 @@
-import neo4j from "neo4j-driver";
+import Neode from "neode";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function connect() {
   try {
-    const driver = neo4j.driver(
-      "bolt://neo4j",
-      neo4j.auth.basic("neo4j", "neo4j")
-    );
+    //   const instance = Neode.fromEnv()
+    const instance = new Neode('bolt://neo4j:7687', 'neo4j', 'neo4j')
+    const result = await instance.with({
+      Person: instance.model("Person", {
+        person_id: {
+          primary: true,
+          type: "uuid",
+          required: true, // Creates an Exists Constraint in Enterprise mode
+        },
+        payroll: {
+          type: "number",
+          unique: "true", // Creates a Unique Constraint
+        },
+        name: {
+          type: "name",
+          index: true, // Creates an Index
+        },
+        age: "number", // Simple schema definition of property : type
+      }),
+    });
+    console.log(result)
 
-    const session = driver.session();
-
-    //  session.run("ALTER CURRENT USER SET PASSWORD FROM 'neo4j' TO 'password")
-
-    session
-      .run("MATCH (n) RETURN (n)")
-      .then((result) => {
-        console.log("RESULT:" + result);
-      })
-      .catch((error) => {
-        console.log("CATCH_ERROR:" + error);
-      })
-      .then(() => {
-        session.close();
-      });
-
-    await driver.close();
-    console.log("IM HERE");
+    const result2 = await instance.cypher("MATCH (p:Person) RETURN p")
+    console.log(result2)
   } catch (error) {
-    console.log("ERROR: " + error);
+    console.log(error);
   }
 }
 
